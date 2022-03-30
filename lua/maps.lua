4d5d638fcef13ffe040de82ee432aa3ba5c2717c
cristian <Heavyblade@users.noreply.github.com>
2022-03-29 22:02:54 -0500

    Adding keymaps to whichkey

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,7 +1,9 @@
+local whichkey = require "which-key"
 local map = vim.api.nvim_set_keymap
 
 vim.g.mapleader = " "
 
+-- General
 map("n", "<SPACE>", "", {})
 map("n", "q", "", {})
 map("n", "<C-w>", ":bd<cr>", {})
@@ -11,18 +13,43 @@ map("n", "<Leader>rr", ":WinResizerStartResize<cr>", {noremap = true})
 map("n", "<Leader>nt", ":NERDTreeToggle<cr>", {noremap = true})
 map("n", "<leader>nf", ":NERDTreeFind<cr>", {noremap = true})
 map("n", "<Leader>af", ":AgitFile<cr>", {noremap = true})
-map("n", "<Leader>tb", ":Telescope buffers<cr>", {noremap = true})
-map("n", "<Leader>tt", ":Telescope current_buffer_tags<cr>", {noremap = true})
-map("n", "<Leader>tg", ":Telescope live_grep<cr>", {noremap = true})
-map("n", "<Leader>tc", ":Telescope git_bcommits<cr>", {noremap = true})
-map("n", "<Leader>ts", ":Telescope git_status<cr>", {noremap = true})
-map("n", "gl", ":HopLine<cr>", {noremap = true, silent = true})
-map("n", "gw", ":HopWord<cr>", {noremap = true, silent = true})
 map("n", "<Leader>rt", ":FloatermNew bundle exec ruby -Itest % -n <cword><cr>", {noremap = true})
-map("n", "<Leader>hm", ":lua require(\"harpoon.mark\").add_file()<cr>", {noremap = true})
-map("n", "<Leader>hg", ":lua require(\"harpoon.ui\").toggle_quick_menu()<cr>", {noremap = true})
-map("n", "<Leader>hn", ":lua require(\"harpoon.ui\").nav_next()<cr>", {noremap = true})
-map("n", "<Leader>hp", ":lua require(\"harpoon.ui\").nav_prev()<cr>", {noremap = true})
+
+-- HopLine
+local keymap_g = {
+  g = {
+    name = "HopLine",
+    l = { "<cmd>:HopLine<CR>", "HopLine Line" },
+    w = { "<cmd>:HopWord<CR>", "HopLine Word" },
+  }
+}
+whichkey.register(keymap_g, { noremap = true, silent = true })
+
+-- Telescope
+local keymap_t = {
+  t = {
+    name = "Telescope",
+    b = { "<cmd>:Telescope buffers<CR>", "Telescope Buffers" },
+    t = { "<cmd>:Telescope current_buffer_tags<CR>", "Telescope Tags" },
+    g = { "<cmd>:Telescope live_grep<CR>", "Telescope Grep" },
+    c = { "<cmd>:Telescope git_bcommits<CR>", "Telescope Commits" },
+    s = { "<cmd>:Telescope git_status<CR>", "Telescope Status" },
+  }
+}
+whichkey.register(keymap_t, { prefix = "<leader>", noremap = true })
+
+-- Harpoon
+local harpoon_ui = require("harpoon.ui")
+local keymap_h = {
+  h = {
+    name = "Harpoon",
+    g = { harpoon_ui.toggle_quick_menu, "Harpoon menu" },
+    m = { harpoon_ui.add_file, "Harpoon mark" },
+    n = { harpoon_ui.nav_next, "Harpoon Next" },
+    p = { harpoon_ui.nav_prev, "Harpoon Prev" },
+  }
+}
+whichkey.register(keymap_h, { prefix = "<leader>", noremap = true })
 
 -- Use ctrl-[hjkl] to select the active split!
 map("n", "<c-k>", ":wincmd k<CR>", {silent = true})
