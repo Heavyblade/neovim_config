54ca61a4c4f0b76cb365a5fb4f63c0cb17a03ef4
cristian <Heavyblade@users.noreply.github.com>
2022-12-19 14:47:07 -0500

    Refactor treesitter config and adding keymap for TrueZen

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ require('plugins')
 require('config.initialize.kanagawa')
 require('config.lsp')
 
-vim.cmd("syntax on")
 vim.cmd('colorscheme kanagawa')
 
 vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
--- lua/config/initialize/treesitter.lua
+++ lua/config/initialize/treesitter.lua
@@ -5,44 +5,45 @@ parser_configs.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
 parser_configs.norg = {
   install_info = {
     url = 'https://github.com/nvim-neorg/tree-sitter-norg',
-    files = {'src/parser.c', 'src/scanner.cc'},
+    files = { 'src/parser.c', 'src/scanner.cc' },
     branch = 'main',
   },
-  maintainers = {'@nvim-neorg'},
+  maintainers = { '@nvim-neorg' },
 }
 parser_configs.norg_meta = {
   install_info = {
     url = 'https://github.com/nvim-neorg/tree-sitter-norg-meta',
-    files = {'src/parser.c'},
+    files = { 'src/parser.c' },
     branch = 'main',
   },
-  maintainers = {'@nvim-neorg'},
+  maintainers = { '@nvim-neorg' },
 }
 parser_configs.norg_table = {
   install_info = {
     url = 'https://github.com/nvim-neorg/tree-sitter-norg-table',
-    files = {'src/parser.c'},
+    files = { 'src/parser.c' },
     branch = 'main',
   },
-  maintainers = {'@nvim-neorg'},
+  maintainers = { '@nvim-neorg' },
 }
 
 require('nvim-treesitter.configs').setup {
-    highlight = {
-        enable = true,
-        disable = {},
-    },
-    indent = {
-        enable = true,
-        disable = {},
-    },
-    ensure_installed = {
-        "ruby",
-        "go",
-        "javascript",
-        "html",
-        "tsx",
-        "norg",
-        "lua",
-    },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "ruby",
+    "go",
+    "javascript",
+    "html",
+    "tsx",
+    "norg",
+    "lua",
+    "markdown",
+  },
 }
