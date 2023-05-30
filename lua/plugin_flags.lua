062f466bff7bbe67b28515166502d69194b85b37
cristian <Heavyblade@users.noreply.github.com>
2023-05-30 14:58:33 -0500

    Remove unnecesary flags

--- lua/plugin_flags.lua
+++ lua/plugin_flags.lua
@@ -1,21 +1,3 @@
--- Ack.vim
-    -- Use ripgrep for searching ⚡️
-    -- Options include:
-    -- --vimgrep -> Needed to parse the rg response properly for ack.vim
-    -- --type-not sql -> Avoid huge sql file dumps as it slows down the search
-    -- --smart-case -> Search case insensitive if all lowercase pattern, Search case sensitively otherwise
-    vim.g['ackprg'] = 'rg --vimgrep --type-not sql --smart-case'
-
-    -- Auto close the Quickfix list after pressing '<enter>' on a list item
-    vim.g['ack_autoclose'] = 1
-
-    -- Any empty ack search will search for the work the cursor is on
-    vim.g['ack_use_cword_for_empty_search'] = 1
-
-    -- Don't jump to first match
-    vim.cmd 'cnoreabbrev Te Telescope'
-    vim.cmd 'cnoreabbrev Ft FloatermNew'
-
 -- Gist
    vim.g['gist_post_private'] = 1
 
@@ -26,10 +8,6 @@
 
 -- Vim-test
    vim.g['test#strategy'] = 'neovim'
-
--- Tokyio Night
-   vim.g.tokyonight_colors = {bg = "#16161e"}
-   vim.g.tokyonight_transparent = true
 
 -- Floatterm
    vim.g.floaterm_autoclose = 0
