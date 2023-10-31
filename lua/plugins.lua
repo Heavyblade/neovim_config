2c16c930bde77fc842b15995ebfd2068ecc41662
cristian <Heavyblade@users.noreply.github.com>
2023-10-31 10:35:44 -0500

    lazy plugin installation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,45 +1,49 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  -- General
-  use {
+local plugins = {
+  {
     'windwp/nvim-autopairs',
     config = function()
       require("nvim-autopairs").setup {
         disable_filetype = { "TelescopePrompt", "norg" }
       }
     end
-  }
-  use {
+  },
+  {
     'numToStr/Comment.nvim',
     config = function()
       require('Comment').setup()
     end
-  }
-  use 'simeji/winresizer'
-  use {
+  },
+  'simeji/winresizer',
+  {
     'phaazon/hop.nvim',
     config = function()
       require('hop').setup()
     end
-  }
-  use 'ThePrimeagen/harpoon'
-  use 'tpope/vim-surround'
-  use {
+  },
+  'ThePrimeagen/harpoon',
+  'tpope/vim-surround',
+  {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
       require("ibl").setup({
-       indent = { char = "│" },
+        indent = { char = "│" },
       })
     end
-  }
-
-  -- Colors
-  use {
+  },
+  {
     'folke/tokyonight.nvim',
     branch = 'main',
     config = function()
@@ -48,104 +52,84 @@ return require('packer').startup(function(use)
         transparent = true,
       })
     end
-  }
-  use "rebelot/kanagawa.nvim"
-
-  -- Git
-  use 'cohama/agit.vim'
-  use { 'airblade/vim-gitgutter', branch = 'main' }
-  use 'tpope/vim-fugitive'
-  use "sindrets/diffview.nvim"
-
-  -- UI
-  use {
+  },
+  "rebelot/kanagawa.nvim",
+  'cohama/agit.vim',
+  { 'airblade/vim-gitgutter', branch = 'main' },
+  'tpope/vim-fugitive',
+  "sindrets/diffview.nvim",
+  {
     'nvim-lualine/lualine.nvim',
     config = function()
       require('config.initialize.lualine')
     end
-  }
-  use {
+  },
+  {
     'nvim-tree/nvim-tree.lua',
     config = function()
       require('config.initialize.nvim_tree')
     end,
-    requires = {
+    dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
-  }
-  use {
+  },
+  {
     'SmiteshP/nvim-navic',
     config = function()
       require('config.initialize.navic')
     end,
-    requires = 'neovim/nvim-lspconfig',
-  }
-  -- Languages
-  use 'fatih/vim-go'
-  use 'elzr/vim-json'
-  use 'vim-test/vim-test'
-  use 'voldikss/vim-floaterm'
-
-  -- Gist vim
-  use 'mattn/vim-gist'
-  use 'mattn/webapi-vim'
-
-  -- Telescope
-  use {
+    dependencies = 'neovim/nvim-lspconfig',
+  },
+  'fatih/vim-go',
+  'elzr/vim-json',
+  'vim-test/vim-test',
+  'voldikss/vim-floaterm',
+  'mattn/vim-gist',
+  'mattn/webapi-vim',
+  {
     'nvim-telescope/telescope.nvim',
     config = function()
       require('config.initialize.telescope')
     end,
-    requires = {
+    dependencies = {
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-treesitter/nvim-treesitter' },
-      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     }
-  }
-
-  -- LSP
-  use {
+  },
+  {
     "neovim/nvim-lspconfig",
-    opt = true,
+    lazy = true,
     event = "BufReadPre",
-    wants = {
-      "mason.nvim",
-      "mason-lspconfig.nvim",
-      "mason-tool-installer.nvim",
-    },
     config = function()
       require("config.lsp").setup()
     end,
-    requires = {
+    dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
-  }
-
-  use {
+  },
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
+    build = ':TSUpdate',
     config = function()
       require("config.initialize.treesitter")
     end,
-    requires = { "nvim-treesitter/playground" }
-  }
-
-  use {
+    dependencies = { "nvim-treesitter/playground" }
+  },
+  {
     'tami5/lspsaga.nvim',
     config = function()
       require('lspsaga').init_lsp_saga()
     end
-  }
-
-  use {
+  },
+  {
     "hrsh7th/nvim-cmp",
     config = function()
       require("config.cmp").setup()
     end,
-    wants = { "LuaSnip" },
-    requires = {
+    dependencies = {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-nvim-lua",
@@ -155,28 +139,25 @@ return require('packer').startup(function(use)
       "hrsh7th/cmp-nvim-lsp",
       {
         "L3MON4D3/LuaSnip",
-        wants = "friendly-snippets",
+        dependencies = { "friendly-snippets" },
         config = function()
           require("config.luasnip").setup()
         end,
       },
       "rafamadriz/friendly-snippets",
-    },
-    disable = false,
-  }
-
-  use 'folke/which-key.nvim'
-  use {
+    }
+  },
+  'folke/which-key.nvim',
+  {
     'folke/trouble.nvim',
     config = function()
       require('trouble').setup {}
     end,
-    requires = { 'kyazdani42/nvim-web-devicons' },
-  }
-
-  use {
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+  },
+  {
     'pwntester/octo.nvim',
-    requires = {
+    dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope.nvim',
       'kyazdani42/nvim-web-devicons',
@@ -184,16 +165,14 @@ return require('packer').startup(function(use)
     config = function()
       require "octo".setup()
     end
-  }
-
-  use({
+  },
+  {
     "Pocco81/true-zen.nvim",
     config = function()
       require("true-zen").setup {}
-    end,
-  })
-
-  use({
+    end
+  },
+  {
     "natecraddock/workspaces.nvim",
     config = function()
       require("workspaces").setup({
@@ -202,16 +181,17 @@ return require('packer').startup(function(use)
           open = { "Telescope find_files" },
         }
       })
-    end,
-  })
-
-  use "github/copilot.vim"
-
-  use {
+    end
+  },
+  "github/copilot.vim",
+  {
     "rest-nvim/rest.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("config.initialize.rest")
     end
   }
-end)
+}
+
+require("lazy").setup(plugins, {})
+
