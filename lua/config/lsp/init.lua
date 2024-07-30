117cb6ace81c796fcbbcf189fa7bda3f33c2ef55
cristian <Heavyblade@users.noreply.github.com>
2024-07-30 12:39:49 -0500

    working with ruby_lsp

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -2,41 +2,35 @@ local M = {}
 local nvim_lsp = require('lspconfig')
 
 local servers = {
-  gopls = {},
-  html = {
+  gopls    = {},
+  html     = {
     filetypes = { 'html', 'erb' },
   },
-  jsonls = {},
-  lua_ls = {},
+  jsonls   = {},
+  lua_ls   = {},
   tsserver = {},
-  bashls = {},
-  vimls = {},
-  pyright = {},
-  solargraph = {
-    cmd = { os.getenv("HOME") .. "/.rbenv/shims/solargraph", 'stdio' },
+  bashls   = {},
+  vimls    = {},
+  pyright  = {},
+  marksman = {},
+  ruby_lsp = {
+    cmd = { os.getenv("HOME") .. "/.rbenv/shims/ruby-lsp", 'stdio' },
     root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git"),
-    settings = {
-      solargraph = {
-        autoformat = true,
-        completion = true,
-        diagnostic = true,
-        folding = true,
-        references = true,
-        rename = true,
-        symbols = true
-      }
+    timeout_ms = 3200,
+    formatting = {
+      timeout_ms = 3200,
     }
   },
-  marksman = {},
 }
 
 function M.setup()
   require("mason").setup({})
   require("mason-lspconfig").setup {
-    -- ensure_installed = vim.tbl_keys(servers),
+    ensure_installed = vim.tbl_keys(servers),
     automatic_installation = false,
   }
   require("config.lsp.configurer").setup(servers)
 end
 
+vim.diagnostic.config({ virtual_text = false })
 return M
