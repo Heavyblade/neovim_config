de4865e38b3c31fa1bfff389ec6309aaee147c2b
cristian <Heavyblade@users.noreply.github.com>
2024-08-09 12:07:56 -0500

    Change startify

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,7 +33,7 @@ local plugins = {
   {
     'goolord/alpha-nvim',
     config = function()
-      require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+      require 'alpha'.setup(require 'alpha.themes.startify'.config)
     end
   },
   'simeji/winresizer',
