91c044478a2cd3655a3c38fa1374ac8c01385322
Cristian Vasquez <cristian.vasquez@dealmaker.tech>
2026-07-17 10:02:23 -0500

    Updating plugings and re-ordering

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,111 +20,7 @@ function canUseCopilot()
 end
 
 local plugins = {
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup {
-        disable_filetype = { "TelescopePrompt", "norg", "typr" }
-      }
-    end
-  },
-  {
-    'goolord/alpha-nvim',
-    config = function()
-      require("config.initialize.alpha-nvim")
-    end
-  },
-  {
-    'smoka7/hop.nvim',
-    event = "BufEnter",
-    lazy = true,
-    config = function()
-      require('hop').setup()
-    end
-  },
-  'ThePrimeagen/harpoon',
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    branch = "main",
-    lazy = true,
-    config = function()
-      require("config.initialize.nvim_treesitter_textobjects")
-    end
-  },
-  {
-    'tpope/vim-surround',
-    event = "InsertEnter"
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      require("ibl").setup({
-        indent = { char = "│" },
-      })
-    end
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    config = function()
-      require('config.initialize.kanagawa')
-    end
-  },
-  { 'airblade/vim-gitgutter',  branch = 'main' },
-  'tpope/vim-fugitive',
-  {
-    'nvim-lualine/lualine.nvim',
-    config = function()
-      require('config.initialize.lualine')
-    end
-  },
-  {
-    'nvim-tree/nvim-tree.lua',
-    config = function()
-      require('config.initialize.nvim_tree')
-    end,
-    dependencies = {
-      'kyazdani42/nvim-web-devicons',
-    },
-  },
-  {
-    'SmiteshP/nvim-navic',
-    lazy = true,
-    config = function()
-      require('config.initialize.navic')
-    end,
-    dependencies = 'neovim/nvim-lspconfig',
-  },
-  {
-    'fatih/vim-go',
-    event = "VeryLazy",
-    lazy = true,
-    ft = 'go',
-  },
-  require("config.initialize.nvim_dap"),
-  {
-    'vim-test/vim-test'
-  },
-  'voldikss/vim-floaterm',
-  { 'akinsho/toggleterm.nvim', version = "*",  config = true },
-  {
-    'mattn/vim-gist',
-    lazy = true,
-    cmd = 'Gist',
-    dependencies = 'mattn/webapi-vim',
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    cmd = "Telescope",
-    config = function()
-      require('config.initialize.telescope')
-    end,
-    dependencies = {
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-treesitter/nvim-treesitter' },
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
-  },
+  -- Basic config
   {
     "neovim/nvim-lspconfig",
     lazy = true,
@@ -173,6 +69,150 @@ local plugins = {
     }
   },
   'folke/which-key.nvim',
+  -- Can't live without these
+  {
+    'nvim-telescope/telescope.nvim',
+    cmd = "Telescope",
+    config = function()
+      require('config.initialize.telescope')
+    end,
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-treesitter/nvim-treesitter' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    }
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+      require('config.initialize.nvim_tree')
+    end,
+    dependencies = {
+      'kyazdani42/nvim-web-devicons',
+    },
+  },
+  {
+    'smoka7/hop.nvim',
+    event = "BufEnter",
+    lazy = true,
+    config = function()
+      require('hop').setup()
+    end
+  },
+  'ThePrimeagen/harpoon',
+  { 'airblade/vim-gitgutter', branch = 'main' },
+  'tpope/vim-fugitive',
+  {
+    'tpope/vim-surround',
+    event = "InsertEnter"
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("ibl").setup({
+        indent = { char = "│" },
+      })
+    end
+  },
+  -- Nice to have
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    config = function()
+      require("render-markdown").setup()
+    end,
+    opts = {
+      file_types = { "markdown", "Avante", "copilot-chat" },
+      ft = { "markdown", "Avante", "copilot-chat" },
+    }
+  },
+  { 'cohama/agit.vim' },
+  {
+    'mattn/vim-gist',
+    lazy = true,
+    cmd = 'Gist',
+    dependencies = 'mattn/webapi-vim',
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup {
+        disable_filetype = { "TelescopePrompt", "norg", "typr" }
+      }
+    end
+  },
+  {
+    'goolord/alpha-nvim',
+    config = function()
+      require("config.initialize.alpha-nvim")
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    lazy = true,
+    config = function()
+      require("config.initialize.nvim_treesitter_textobjects")
+    end
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require('config.initialize.kanagawa')
+    end
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('config.initialize.lualine')
+    end
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<Tab>",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "main",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    opts = require("config.initialize.copilot_chat").opts,
+  },
+  -- Maybe can go without noticing
+  {
+    'SmiteshP/nvim-navic',
+    lazy = true,
+    config = function()
+      require('config.initialize.navic')
+    end,
+    dependencies = 'neovim/nvim-lspconfig',
+  },
+  {
+    'fatih/vim-go',
+    event = "VeryLazy",
+    lazy = true,
+    ft = 'go',
+  },
+  require("config.initialize.nvim_dap"),
+  {
+    'vim-test/vim-test'
+  },
+  'voldikss/vim-floaterm',
+  { 'akinsho/toggleterm.nvim', version = "*", config = true },
   {
     'folke/trouble.nvim',
     cmd = "TroubleToggle",
@@ -206,55 +246,11 @@ local plugins = {
     ft = { "http", "rest" },
   },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          keymap = {
-            accept = "<Tab>",
-          },
-        },
-      })
-    end,
-  },
-  {
-    "nvzone/typr",
-    dependencies = "nvzone/volt",
-    opts = {},
-    cmd = { "Typr", "TyprStats" },
-  },
-  { 'cohama/agit.vim' },
-  { "sindrets/diffview.nvim" },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" },
-      { "nvim-lua/plenary.nvim" },
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    requires = {
+      "nvim-lua/plenary.nvim",
     },
-    opts = require("config.initialize.copilot_chat").opts,
-  },
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    config = function()
-      require("render-markdown").setup()
-    end,
-    opts = {
-      file_types = { "markdown", "Avante", "copilot-chat" },
-      ft = { "markdown", "Avante", "copilot-chat" },
-    }
-  },
-  {
-    "eltonsst/local-review.nvim",
-    config = function()
-      require("local_review").setup({
-        keymap = "<leader>rc",
-      })
-    end,
   }
 }
 
